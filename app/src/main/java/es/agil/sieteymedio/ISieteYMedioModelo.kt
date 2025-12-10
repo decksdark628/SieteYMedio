847663a9257eca0190ff47f76bdc85b7089b197d
DecksDark <patrickdiazgrandez@gmail.com>
2025-12-10 12:06:28 +0100

    finished outline of model class and interface

--- app/src/main/java/es/agil/sieteymedio/ISieteYMedioModelo.kt
+++ app/src/main/java/es/agil/sieteymedio/ISieteYMedioModelo.kt
@@ -1,5 +1,18 @@
 package es.agil.sieteymedio
 
 interface ISieteYMedioModelo{
+    fun createCards()
+    fun shuffleCards()
 
+    fun nextCard()
+    fun getNextCard():SieteYMedioModelo.Carta
+    fun resetCurrentCardPos()
+
+    fun setTurno(player:Int)
+    fun getTurno():Int
+
+    fun addPuntaje(player:Int, value:Double)
+    fun getPuntaje(player:Int):Double
+
+    fun resetPuntajes()
 }
--- app/src/main/java/es/agil/sieteymedio/SieteYMedioModelo.kt
+++ app/src/main/java/es/agil/sieteymedio/SieteYMedioModelo.kt
@@ -8,6 +8,15 @@ class SieteYMedioModelo{
         BASTOS
     }
 
+    data class Carta(
+        val set: Set,
+        val count: Int,
+        val value: Double,
+        val lectura: String,
+        val img: String
+    )
+    //TODO: Se puede sobreescribir el toString de este data class?
+
     val stringValues = mapOf(
         1 to "Uno",
         2 to "Dos",
@@ -21,11 +30,12 @@ class SieteYMedioModelo{
         10 to "Rey"
     )
 
-    data class Carta(
-        val set: Set,
-        val count: Int,
-        val value: Double,
-        val lectura: String,
-        val img: String
-    )
+    val rulesText:String = "Lorem ipsum"
+    val hiddenCardImgPath:String ="hidden.png"
+
+    var turno:Int = 1
+    var currentDeckPosition:Int = 0
+    var puntajeJ1:Double = 0.0
+    var puntajeJ2:Double = 0.0
+    lateinit var deck:List<Carta>
 }
