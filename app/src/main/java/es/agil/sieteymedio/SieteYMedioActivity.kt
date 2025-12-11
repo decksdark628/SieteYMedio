a295d6142bb83922af619fff3595e263df434f65
DecksDark <134467274+decksdark628@users.noreply.github.com>
2025-12-11 12:15:39 +0100

    added gradle viewBinding

--- app/src/main/java/es/agil/sieteymedio/SieteYMedioActivity.kt
+++ app/src/main/java/es/agil/sieteymedio/SieteYMedioActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SieteYMedioActivity : AppCompatActivity(){
+class SieteYMedioActivity : ISieteYMedioVista, AppCompatActivity(){
     val hiddenCardImgPath:String ="hidden.png"
     val rulesText:String = "Lorem ipsum"
 
@@ -19,5 +19,49 @@ class SieteYMedioActivity : AppCompatActivity(){
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+    }
+
+    override fun enableCard(player: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun disableCard(player: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideCard(player: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showCard(player: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun enablePlantarseButton(player: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun disablePlantarseButton(player: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCurrentSumValue(player: Int, value: Double) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setWinnerText(text: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showWinnerText() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideWinnerText() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showWinnerGif() {
+        TODO("Not yet implemented")
     }
 }
